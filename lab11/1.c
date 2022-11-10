332860d189c789c41c7c56a9995e722f66d6b1f3
biswaroopnath <biswaroopnath2001@gmail.com>
2022-11-10 10:19:47 +0530

    hwllo womld

--- lab11/1.c
+++ lab11/1.c
@@ -1,170 +1,68 @@
-//bfs traversal
-// BFS algorithm in C
-
-#include <stdio.h>
-#include <stdlib.h>
-#define SIZE 40
-
-struct queue {
-  int items[SIZE];
-  int front;
-  int rear;
-};
-
-struct queue* createQueue();
-void enqueue(struct queue* q, int);
-int dequeue(struct queue* q);
-void display(struct queue* q);
-int isEmpty(struct queue* q);
-void printQueue(struct queue* q);
-
-struct node {
-  int vertex;
-  struct node* next;
-};
-
-struct node* createNode(int);
-
-struct Graph {
-  int numVertices;
-  struct node** adjLists;
-  int* visited;
-};
-
-// BFS algorithm
-void bfs(struct Graph* graph, int startVertex) {
-  struct queue* q = createQueue();
-
-  graph->visited[startVertex] = 1;
-  enqueue(q, startVertex);
-
-  while (!isEmpty(q)) {
-    printQueue(q);
-    int currentVertex = dequeue(q);
-    printf("Visited %d\n", currentVertex);
-
-    struct node* temp = graph->adjLists[currentVertex];
-
-    while (temp) {
-      int adjVertex = temp->vertex;
-
-      if (graph->visited[adjVertex] == 0) {
-        graph->visited[adjVertex] = 1;
-        enqueue(q, adjVertex);
-      }
-      temp = temp->next;
-    }
-  }
-}
-
-// Creating a node
-struct node* createNode(int v) {
-  struct node* newNode = malloc(sizeof(struct node));
-  newNode->vertex = v;
-  newNode->next = NULL;
-  return newNode;
-}
-
-// Creating a graph
-struct Graph* createGraph(int vertices) {
-  struct Graph* graph = malloc(sizeof(struct Graph));
-  graph->numVertices = vertices;
-
-  graph->adjLists = malloc(vertices * sizeof(struct node*));
-  graph->visited = malloc(vertices * sizeof(int));
-
-  int i;
-  for (i = 0; i < vertices; i++) {
-    graph->adjLists[i] = NULL;
-    graph->visited[i] = 0;
-  }
-
-  return graph;
-}
-
-// Add edge
-void addEdge(struct Graph* graph, int src, int dest) {
-  // Add edge from src to dest
-  struct node* newNode = createNode(dest);
-  newNode->next = graph->adjLists[src];
-  graph->adjLists[src] = newNode;
-
-  // Add edge from dest to src
-  newNode = createNode(src);
-  newNode->next = graph->adjLists[dest];
-  graph->adjLists[dest] = newNode;
-}
-
-// Create a queue
-struct queue* createQueue() {
-  struct queue* q = malloc(sizeof(struct queue));
-  q->front = -1;
-  q->rear = -1;
-  return q;
-}
-
-// Check if the queue is empty
-int isEmpty(struct queue* q) {
-  if (q->rear == -1)
-    return 1;
-  else
+#include<stdio.h>
+#include<stdlib.h>
+ 
+struct queue { int size; int f; int r; int* arr; };
+ 
+int isEmpty(struct queue *q){
+    if(q->r == q->f) return 1;
     return 0;
 }
-
-// Adding elements into queue
-void enqueue(struct queue* q, int value) {
-  if (q->rear == SIZE - 1)
-    printf("\nQueue is Full!!");
-  else {
-    if (q->front == -1)
-      q->front = 0;
-    q->rear++;
-    q->items[q->rear] = value;
-  }
+ 
+int isFull(struct queue *q){
+    if(q->r==q->size-1) return 1;
+    return 0;
 }
-
-// Removing elements from queue
-int dequeue(struct queue* q) {
-  int item;
-  if (isEmpty(q)) {
-    printf("Queue is empty");
-    item = -1;
-  } else {
-    item = q->items[q->front];
-    q->front++;
-    if (q->front > q->rear) {
-      printf("Resetting queue ");
-      q->front = q->rear = -1;
+ 
+void enqueue(struct queue *q, int val){
+    if(isFull(q)) printf("This Queue is full\n");
+    else{
+        q->r++;
+        q->arr[q->r] = val;
     }
-  }
-  return item;
 }
-
-// Print the queue
-void printQueue(struct queue* q) {
-  int i = q->front;
-
-  if (isEmpty(q)) {
-    printf("Queue is empty");
-  } else {
-    printf("\nQueue contains \n");
-    for (i = q->front; i < q->rear + 1; i++) {
-      printf("%d ", q->items[i]);
+ 
+int dequeue(struct queue *q){
+    int a = -1;
+    if(isEmpty(q)) printf("This Queue is empty\n");
+    else{
+        q->f++;
+        a = q->arr[q->f]; 
     }
-  }
+    return a;
 }
-
-int main() {
-  struct Graph* graph = createGraph(6);
-  addEdge(graph, 0, 1);
-  addEdge(graph, 0, 2);
-  addEdge(graph, 1, 2);
-  addEdge(graph, 1, 4);
-  addEdge(graph, 1, 3);
-  addEdge(graph, 2, 4);
-  addEdge(graph, 3, 4);
-
-  bfs(graph, 0);
-
-  return 0;
+ 
+int main(){
+    // Initialize Queue
+    struct queue q;
+    q.size = 400;
+    q.f = q.r = 0;
+    q.arr = (int*) malloc(q.size*sizeof(int));
+    
+    // BFS
+    int node;
+    int i = 1;
+    int visited[7] = {0,0,0,0,0,0,0};
+    int a [7][7] = {
+        {0,1,1,1,0,0,0},
+        {1,0,1,0,0,0,0},
+        {1,1,0,1,1,0,0},
+        {1,0,1,0,1,0,0},
+        {0,0,1,1,0,1,1},
+        {0,0,0,0,1,0,0}, 
+        {0,0,0,0,1,0,0} 
+    };
+    printf("%d ", i);
+    visited[i] = 1;
+    enqueue(&q, i);
+    while (!isEmpty(&q)) {
+        int node = dequeue(&q);
+        for (int j = 0; j < 7; j++) {
+            if(a[node][j] == 1 && visited[j] == 0){
+                printf("%d ", j);
+                visited[j] = 1;
+                enqueue(&q, j);
+            }
+        }
+    }
+    return 0;
 }
